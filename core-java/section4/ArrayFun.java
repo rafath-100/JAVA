89ebe10ed3b0f2082f119a1ebdfd382a1d0a384d
Mir Rafath Ali <mirrafathali100@gmail.com>
2022-01-22 20:08:52 +0530

    Arrays started

---
+++ core-java/section4/ArrayFun.java
@@ -0,0 +1,18 @@
+//import java.util.*;
+class ArrayFun {
+    public static void main(String args[])
+    {
+        int[] myArray;
+        myArray=new int[5];
+
+        myArray[0]=5;
+        myArray[1]=10;
+        myArray[2]=15;
+        myArray[3]=20;
+        myArray[4]=25;
+
+        for(int i=0;i<myArray.length;i++){
+            System.out.println(myArray[i]);
+        }
+    }
+}
