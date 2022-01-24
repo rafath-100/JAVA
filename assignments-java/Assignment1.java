0559908af0b8a0e3de8d4a553cf80b2736903671
Mir Rafath Ali <mirrafathali100@gmail.com>
2022-01-24 15:34:23 +0530

    assignment 1 modified

--- assignments-java/Assignment1.java
+++ assignments-java/Assignment1.java
@@ -1,95 +1,50 @@
-// import java.util.Scanner; 
-// import java.util.regex.*; 
-// import java.io.File; 
-
-// public class Assignment1 
-// {
-//     public static void main(String args[])
-//     {
-//         Scanner keyboard=new Scanner(System.in);
-//         File file;
-//         File[] paths;
-//         try{
-//             String fileName;
-//             boolean flag=false;
-
-//             file=new File("/home/Desktop/clean-code/SRP.java");
-//             paths=file.listFiles();
-
-//             System.out.println("Enter file name or terminate the process: ");
-//             fileName=keyboard.next();
-
-//             while(!fileName.equals("exit"))
-//             {
-//                 Pattern pattern=Pattern.compile(fileName);
-
-//                 for(File path: paths)
-//                 {
-//                     Matcher matcher = pattern.matcher(path.getName());
-//                     flag = matcher.matches();
-//                     if(flag)
-//                     {
-//                         System.out.println("Full Path: "+ path.getAbsolutePath());
-//                         break;
-//                     }
-//                 }
-//                 if(!flag)
-//                 {
-//                     System.out.println(fileName+ " Not found");
-//                 }
-//                 System.out.println("Enter file name or terminate the process: ");
-//                 fileName = keyboard.next();
-//             }
-//         }catch(Exception e){
-//             e.printStackTrace();
-//         }
-//     }
-// }
-
+//Create a java program to search through the home directory and look for files that match a regular expression. The program should be able to take inputs repeatedly and should print out the full absolute path of the matching files found.
+ 
 import java.io.File;
 import java.util.Scanner;
-import java.util.regex.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Assignment1 {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        boolean matchFound=false;
+        Scanner keyboard = new Scanner(System.in);
+        do {
+            System.out.println("enter the name of the file");
 
-        File file;
-        File[] paths;
+            String input = keyboard.nextLine();
 
-        try {
-            String fileName;
-            boolean flag = false;
+            //here we are entering a file name to search in a particular directory
+            File file = new File("/home/rafatam/Desktop");
+            String[] fileList = file.list();
 
-            file = new File("/home/Desktop/clean-code");
-            paths = file.listFiles();
-
-            System.out.println("Enter the file name as regular expression  or type exit to terminate : ");
-            fileName = input.next();
-
-            while(!fileName.equals("exit")){
-                Pattern pattern = Pattern.compile(fileName);
-
-                for(File path : paths){
-                    Matcher matcher = pattern.matcher(path.getName());
-                    flag = matcher.matches();
-                    if(flag){
-                        System.out.println("Full Absolute Path : " + path.getAbsolutePath());
-                        break;
-                    }
-                }
-
-                if(!flag){
-                    System.out.println(fileName + " Not Found");
-                }
-
-                System.out.println("Enter the file name as regular expression or type exit to terminate : ");
-                fileName = input.next();
+            for (String fileNames : fileList) {
+                // Creating a pattern of the file to be searched
+                Pattern pattern = Pattern.compile(input);
+                // Searching above pattern in the list of files
+                Matcher matcher = pattern.matcher(fileNames);
+                matchFound = matcher.find();
+                //if match is found come out of the loop
+                if (matchFound)
+                    break;
             }
 
-        }catch(Exception e){
-            e.printStackTrace();
-        }
+            if (matchFound) {
+                System.out.println("Match found - Here is the absolute path =>" + file.getAbsolutePath());
+            } else {
+                System.out.println("Sorry, the Match is not there :(");
+            }
+
+            File[] paths;
+            String[] pathnames;
+            paths = file.listFiles();
+            pathnames=file.list();
+
+            //with the following loop will be able to print all the files that are present in that directory or folder.
+            for(String pathname : pathnames ){
+                System.out.println(pathname);
+            }
+        }  while (true) ;
     }
 }
