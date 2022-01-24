ed679b1e52f8ebd576f89537f7e5939c92b3d39c
Mir Rafath Ali <mirrafathali100@gmail.com>
2022-01-24 15:35:17 +0530

    assignment 2 modified

--- assignments-java/Assignment2.java
+++ assignments-java/Assignment2.java
@@ -1,19 +1,19 @@
-import java.util.*;
+
 import java.util.Scanner;
 
 public class Assignment2 {
    public static void main(String[] args) {
-      String str; 
-      Scanner in = new Scanner(System.in);
+      String string1; 
+      Scanner input = new Scanner(System.in);
       System.out.print("\nEnter your String ");
-      str =in.nextLine();
-      str = str.toLowerCase();
+      string1 =input.nextLine();
+      string1 = string1.toLowerCase();
       boolean[] alphaList = new boolean[26]; 
       int index = 0;
       int flag = 1;
-      for (int i = 0; i < str.length(); i++) {
-        if( str.charAt(i) >= 'a' &&  str.charAt(i) <= 'z') {
-            index = str.charAt(i) - 'a'; 
+      for (int i = 0 ; i < string1.length() ; i++) {
+        if( string1.charAt(i) >= 'a' &&  string1.charAt(i) <= 'z') {
+            index = string1.charAt(i) - 'a'; 
          }
          alphaList[index] = true; 
       }
@@ -21,7 +21,7 @@ public class Assignment2 {
          if (alphaList[i] == false)
             flag = 0;
       }
-      System.out.print("String: " + str);
+      System.out.print("String: " + string1);
       if (flag == 1)
          System.out.print("\nThe above string is a pangram.\n ");
       else
